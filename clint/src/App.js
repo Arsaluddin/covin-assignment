5a9db17a97d89fbb9757cb4c330acbd3b0ec3815
arsal uddin <arsaluddin134@gmail.com>
2023-03-29 23:04:13 +0530

    .

--- clint/src/App.js
+++ clint/src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
 import {BrowserRouter, Routes,Route,Link} from "react-router-dom";
 import { useState } from "react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import './App.css';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
 
      const [cards,setCards] = useState([]);
-     
-    const handlecardformation = () => {
-        
-      <form typeof="submit">
-        <input type='text'></input>
-      </form> 
-
-    }
+     const [open, setOpen] = useState(false);
+     const handleOpen = () => setOpen(true);
+     const handleClose = () => setOpen(false);
 
   return (
     <div className="App">
@@ -25,17 +35,27 @@ function App() {
             <button>History</button>
          </Link>
          
-            
-          <button onClick={() => handlecardformation()}>+ Create Card</button>
-            
-         
-       </header>
-        <h1>create your own card</h1>
-       <main>
-         <Routes>
-           <Route />
-         </Routes> 
-       </main> 
+          <div>
+            <Button onClick={handleOpen}>+ Create Card</Button>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+             >
+             <Box sx={style}>
+               <form>
+                <label>card name</label>
+                <input></input>
+                <lable>Description</lable>
+                <input></input>
+               </form>
+             </Box>
+            </Modal>
+          </div>
+        
+        </header>
+        
       </BrowserRouter>
 
     </div>
